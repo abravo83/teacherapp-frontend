b0c9c47ff9a6fd64436264f2f454362700d1e3db
Israel Arenas <israel.djaz@gmail.com>
2024-10-29 22:58:18 +0100

    modificacion-navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  islogin():boolean{
+    return true;
+  }
+
+  logout(){}
+
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { RouterLink } from '@angular/router';
 
 import { ProfesorCardHomeComponent } from '../../components/profesor-list-home/profesor-card-home/profesor-card-home.component';
 import { ProfesorListHomeComponent } from '../../components/profesor-list-home/profesor-list-home.component';
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, ProfesorCardHomeComponent, ProfesorListHomeComponent],
+  imports: [RouterLink, ProfesorCardHomeComponent, ProfesorListHomeComponent, NavbarComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
