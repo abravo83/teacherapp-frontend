b3f79676efdd6ebf39df3291ee9f670a562650c0
Arturo Carrasco González <arturocarrascogonzalez@hotmail.com>
2024-10-31 13:51:22 +0100

    Formulario de registro y actualización de alumno

--- src/app/pages/form-student/form-student.component.ts
+++ src/app/pages/form-student/form-student.component.ts
@@ -1,12 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators, ValidatorFn, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { Iusuario } from '../../interfaces/iusuario';
+import { AlumnosService } from '../../services/alumnos.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form-student',
   standalone: true,
-  imports: [],
   templateUrl: './form-student.component.html',
-  styleUrl: './form-student.component.css'
+  styleUrls: ['./form-student.component.css'],
+  imports: [ReactiveFormsModule, CommonModule]
 })
-export class FormStudentComponent {
+export class FormStudentComponent implements OnInit {
 
+  alumnosService = inject(AlumnosService);
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
+
+  errorForm: any[] = [];
+  tipo: string = 'Registra';
+  studentForm: FormGroup;
+
+  constructor() {
+    this.studentForm = new FormGroup({
+      nombre: new FormControl(null, [Validators.required, Validators.maxLength(45)]),
+      apellidos: new FormControl(null, [Validators.required, Validators.maxLength(150)]),
+      email: new FormControl(null, [Validators.required, Validators.email, Validators.maxLength(60)]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(255),
+        Validators.pattern(/^(?=.*[A-Z])(?=.*\d).{8,}$/)
+      ]),
+      repitepassword: new FormControl(null, [Validators.required]),
+      foto: new FormControl(null, [Validators.maxLength(255), Validators.pattern(/^https?:\/\/.*\.(?:png|jpg|jpeg|webp)$/)])
+    }, { validators: this.validadorCoincidenciaContraseñas });
+  }
+
+  validadorCoincidenciaContraseñas: ValidatorFn = (group: AbstractControl): { [key: string]: any } | null => {
+    const password = group.get('password')?.value;
+    const repitepassword = group.get('repitepassword')?.value;
+    return password === repitepassword ? null : { checkpassword: true };
+  };
+
+  checkControl(formControlName: string, validador: string) {
+    return (
+      this.studentForm.get(formControlName)?.hasError(validador) &&
+      this.studentForm.get(formControlName)?.touched
+    );
+  }
+
+  ngOnInit() {
+    this.activatedRoute.params.subscribe(async (params: any) => {
+      if (params.id) {
+        this.tipo = 'Actualizar';
+        const alumno: Iusuario | undefined = await this.alumnosService.getAlumnoById(Number(params.id));
+        if (alumno) {
+          this.studentForm.patchValue(alumno);
+        }
+      }
+    });
+  }
+
+  async obtenerDatosFormulario() {
+    const formData = {
+      alumno: {
+        ...this.studentForm.value,
+        rol: 'alumno',
+        activo: true
+      }
+    };
+
+    delete formData.alumno.repitepassword;
+
+    console.log("Datos enviados:", formData);
+
+    if (this.studentForm.value.id) {
+      try {
+        const response = await this.alumnosService.actualizarAlumno(formData.alumno);
+        if (response) {
+          alert('Alumno actualizado');
+          this.router.navigate(['/home']);
+        }
+      } catch (error: any) {
+        this.errorForm = error;
+      }
+    } else {
+      try {
+        const response = await this.alumnosService.registroAlumno(formData.alumno);
+        if (response) {
+          this.router.navigate(['/home']);
+        }
+      } catch (error: any) {
+        this.errorForm = error;
+      }
+    }
+  }
 }
--- src/app/services/alumnos.service.ts
+++ src/app/services/alumnos.service.ts
@@ -1,6 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { Iusuario } from '../interfaces/iusuario';
 import { UsuariosService } from './usuarios.service';
+import { USUARIOS } from '../db/usuarios';
 
 @Injectable({
   providedIn: 'root',
@@ -16,4 +17,32 @@ export class AlumnosService {
       (usuario) => usuario.rol === 'alumno'
     );
   }
+
+  // Métodos
+
+  getAlumnoById(id: number): Promise<Iusuario | undefined> {
+    return new Promise((resolve) => {
+      const alumno = USUARIOS.find((usr) => usr.id === id && usr.rol === 'alumno');
+      resolve(alumno);
+    });
+  }
+
+  async registroAlumno(alumno: Iusuario) {
+    alumno.id = USUARIOS.length + 1;
+    USUARIOS.push(alumno);
+    return alumno;
+  }
+
+  async actualizarAlumno(alumno: Iusuario) {
+    const index = USUARIOS.findIndex((usr) => usr.id === alumno.id && usr.rol === 'alumno');
+    if (index !== -1) {
+      USUARIOS[index] = alumno;
+      return alumno;
+    }
+    throw new Error("Alumno no encontrado");
+  }
+
+
+
+
 }
