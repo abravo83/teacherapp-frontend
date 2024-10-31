c2ea7e67674de0d6b8fe6bec2eaa11aff2ed54e6
Alfonso Bravo <alfonso.bravo.molina@gmail.com>
2024-10-31 06:27:03 +0100

    Created new pages and new routes for the rest of the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LogoutComponent } from './pages/logout/logout.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
-  //{path:"login", component:LoginComponent},
-  //{path:"register", component:RegisterComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'home' },
 ];
