905d2e49e2243b7bf73bd5e2ede63c5329e1af04
Isa Zuni <id.zuninoe@gmail.com>
2024-11-02 19:53:09 +0100

    Actualizacion dashboard-roles

--- src/app/profile/profesor/my-classes/my-classes.component.ts
+++ src/app/profile/profesor/my-classes/my-classes.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TeacherDataService } from '../../../services/teacher-data.service';
 
 @Component({
   selector: 'app-my-classes',
   templateUrl: './my-classes.component.html',
-  styleUrl: './my-classes.component.css',
+  styleUrls: ['./my-classes.component.css'],
 })
-export class MyClassesComponent {}
+export class MyClassesComponent {
+  courses$: Observable<any[]>;
+
+  constructor(private teacherDataService: TeacherDataService) {
+    this.courses$ = this.teacherDataService.getScheduledClasses();
+
+    this.courses$.subscribe((data) => {
+      console.log('Datos de las clases programadas:', data);
+    });
+  }
+}
--- src/app/profile/profesor/profesor-dashboard/profesor-dashboard.component.ts
+++ src/app/profile/profesor/profesor-dashboard/profesor-dashboard.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profesor-dashboard',
   templateUrl: './profesor-dashboard.component.html',
-  styleUrl: './profesor-dashboard.component.css',
+  styleUrls: ['./profesor-dashboard.component.css'],
 })
 export class ProfesorDashboardComponent {}
