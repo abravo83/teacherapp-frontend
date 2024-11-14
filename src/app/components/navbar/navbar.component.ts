c3598f19ab5464bb015bcf94341f5aeb5aaf54f3
Israel Arenas <israel.djaz@gmail.com>
2024-11-14 14:14:12 +0100

    count-notificaciones

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,24 +1,73 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { MensajesService } from '../../services/mensajes.service';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  loginService = inject(LoginService);
   login: boolean = false;
-  role:string = 'alumno';
-  notiCount: number = 1
-  
+  role:string = 'alumno';  
+  notifications:Array<{id:number,contenido: string; leido: boolean}>=[];
+  notiCount:number = 0;
 
-  hasnoti():boolean{
-    return this.notiCount>0;
+  showNotifications= false;
+  
+  serviceMensaje= inject(MensajesService);
+  //arrMensajes: []=[];
+
+  /* async ngOnInit(){
+   
+    this.arrMensajes = await this.serviceMensaje.getAll();
+    console.log(this.arrMensajes);
+  } */
+  ngOnInit() {
+    // Cargar las notificaciones llamar al servicio de notificaciones)
+    this.login = this.loginService.isLogged()
+
+    this.notifications = [
+      { id: 1, contenido: 'contenido Mensaje 1', leido: false },
+    { id: 2, contenido: 'contenido Mensaje 2', leido: false },
+    { id: 3, contenido: 'contenido Mensaje 3', leido: false },
+    { id: 4, contenido: 'contenido Mensaje 4', leido: false },
+    { id: 5, contenido: 'contenido Mensaje 5', leido: true },
+    { id: 5, contenido: 'contenido Mensaje 5', leido: true }
+    ];
   }
 
+  //carga numero de notificaciones
+  hasnoti():number{
+    if(this.notifications.length){
+      this.notiCount = this.notifications.filter(msj => !msj.leido).length;
+      return this.notiCount > 0 ?this.notiCount:0;
+    }
+    return 0;
+  }
 
+  //desplega listado de notificaciones
+  toggleNotificaciones():void{
+    this.showNotifications = !this.showNotifications;
+  }
    
+  notificationUnRead(){
+    //let result = this.notifications.filter(mensaje =>!mensaje.leido);
+    return this.notifications.filter(mensaje =>!mensaje.leido);
+  }
+
+  readNotifications(notificacionid:number){
+    const notification = this.notifications.find(n => n.id === notificacionid);
+    if(notification)
+      notification.leido = true;
+  }
+
+  
 
 }
--- src/app/services/mensajes.service.ts
+++ src/app/services/mensajes.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 
 import { Imensaje } from '../interfaces/imensaje';
 import { MENSAJES } from '../db/mensajes';
+import { environment } from '../../environments/environments';
+import { HttpClient } from '@angular/common/http';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +12,15 @@ import { MENSAJES } from '../db/mensajes';
 export class MensajesService {
   arrMensajes: Imensaje[] = MENSAJES;
 
+  private baseUrl: string = `${environment.API_URL}/api/mensajes`;
+  private httpClient = inject(HttpClient);
+
   constructor() {}
+
+  getAll(){
+    console.log(`base url: ${this.baseUrl}`);
+    return lastValueFrom(this.httpClient.get<Imensaje[]>(this.baseUrl));
+  }
+
+
 }
